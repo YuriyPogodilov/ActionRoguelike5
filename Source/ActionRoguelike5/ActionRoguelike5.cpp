631506e53f19c8db36f12f3d4e878016d3bf7d22
Yuriy Pogodilov <pogodilov@gmail.com>
2026-09-07 13:04:08 +0800

    initial

---
+++ Source/ActionRoguelike5/ActionRoguelike5.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ActionRoguelike5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRoguelike5, "ActionRoguelike5" );
