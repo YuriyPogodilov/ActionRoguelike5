bbc6e5e9600eeb7b4a76b748cf56af03623d6fd6
Yuriy Pogodilov <pogodilov@gmail.com>
2026-09-08 16:25:20 +0800

    Basic projectile

---
+++ Source/ActionRoguelike5/Projectiles/RogueProjectileMagic.cpp
@@ -0,0 +1,44 @@
+﻿#include "RogueProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ARogueProjectileMagic::ARogueProjectileMagic()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.f);
+	SphereComponent->SetCollisionProfileName("Projectile");
+	
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
+	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComp"));
+	ProjectileMovementComponent->InitialSpeed = 2000.0f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.f;
+}
+
+void ARogueProjectileMagic::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+	SphereComponent->OnComponentHit.AddDynamic(this, &ARogueProjectileMagic::OnActorHit);
+}
+
+void ARogueProjectileMagic::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// @todo: create our damage class
+	TSubclassOf<UDamageType> DmgTypeClass = UDamageType::StaticClass();
+	
+	UGameplayStatics::ApplyDamage(OtherActor, 10.f, GetInstigatorController(), this, DmgTypeClass);
+	
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ExplosionEffect, GetActorLocation());
+	
+	Destroy();
+}
+
---
+++ Source/ActionRoguelike5/Projectiles/RogueProjectileMagic.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueProjectileMagic.generated.h"
+
+class UNiagaraSystem;
+class USphereComponent;
+class UProjectileMovementComponent;
+class UNiagaraComponent;
+
+UCLASS()
+class ACTIONROGUELIKE5_API ARogueProjectileMagic : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARogueProjectileMagic();
+	
+	void PostInitializeComponents() override;
+	
+protected:
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	TObjectPtr<UNiagaraSystem> ExplosionEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UNiagaraComponent> LoopedNiagaraComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+};
