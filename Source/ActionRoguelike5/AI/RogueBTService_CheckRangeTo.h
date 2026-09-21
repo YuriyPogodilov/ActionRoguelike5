e5b81654f10609e7be71e5033f4cc23c8f9e7587
Yuriy Pogodilov <pogodilov@gmail.com>
2026-09-21 22:59:17 +0700

    AI. Simple service CheckRangeTo

---
+++ Source/ActionRoguelike5/AI/RogueBTService_CheckRangeTo.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/BehaviorTree/BTService.h"
+#include "RogueBTService_CheckRangeTo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE5_API URogueBTService_CheckRangeTo : public UBTService
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector TargetActorKey;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector WithinRangeKey;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	float MaxAttackRange;
+	
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
