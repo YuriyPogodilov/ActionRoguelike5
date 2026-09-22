273ac6a4e68986b8fa087f6731349be1eb35c39b
Yuriy Pogodilov <pogodilov@gmail.com>
2026-09-22 18:36:49 +0700

    Added minion death

--- Source/ActionRoguelike5/AI/RogueAICharacter.cpp
+++ Source/ActionRoguelike5/AI/RogueAICharacter.cpp
@@ -3,7 +3,40 @@
 
 #include "RogueAICharacter.h"
 
+#include "ActionSystem/RogueActionSystemComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+
+void ARogueAICharacter::OnHealthChanged(float NewHealth, float OldHealth)
+{
+	if (FMath::IsNearlyZero(NewHealth))
+	{
+		DetachFromControllerPendingDestroy();
+		
+		GetMovementComponent()->StopMovementImmediately();
+		
+		PlayAnimMontage(DeathMontage);
+	}
+}
 
 ARogueAICharacter::ARogueAICharacter()
 {
+	ActionSystemComponent = CreateDefaultSubobject<URogueActionSystemComponent>(TEXT("ActionSystemComp"));
 }
+
+void ARogueAICharacter::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+	ActionSystemComponent->OnHealthChanged.AddDynamic(this, &ARogueAICharacter::OnHealthChanged);
+}
+
+float ARogueAICharacter::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser)
+{
+	float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+	
+	ActionSystemComponent->ApplyHealthChange(-DamageAmount);
+	
+	return ActualDamage;
+}
+
--- Source/ActionRoguelike5/AI/RogueAICharacter.h
+++ Source/ActionRoguelike5/AI/RogueAICharacter.h
@@ -6,11 +6,28 @@
 #include "GameFramework/Character.h"
 #include "RogueAICharacter.generated.h"
 
+class URogueActionSystemComponent;
+
 UCLASS()
 class ACTIONROGUELIKE5_API ARogueAICharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<URogueActionSystemComponent> ActionSystemComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Death")
+	TObjectPtr<UAnimMontage> DeathMontage;
+	
+	UFUNCTION()
+	void OnHealthChanged(float NewHealth, float OldHealth);
+	
 public:
 	ARogueAICharacter();
+	
+	virtual void PostInitializeComponents() override;
+	
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 };
