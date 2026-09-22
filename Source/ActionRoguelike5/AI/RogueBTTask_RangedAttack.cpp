c313f6650a64e32d8fafdc42bc51326fae7d23ef
Yuriy Pogodilov <pogodilov@gmail.com>
2026-09-22 15:33:29 +0700

    Added RangedAttack BTTask

---
+++ Source/ActionRoguelike5/AI/RogueBTTask_RangedAttack.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueBTTask_RangedAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "Projectiles/RogueProjectile.h"
+
+EBTNodeResult::Type URogueBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	ACharacter* Pawn = Cast<ACharacter>(OwnerComp.GetAIOwner()->GetPawn());
+	check(Pawn);
+	
+	FVector SpawnLocation = Pawn->GetMesh()->GetSocketLocation(MuzzleSocketName);
+	
+	AActor* TargetActor = Cast<AActor>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (!IsValid(TargetActor))
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	FRotator SpawnRotation = (TargetActor->GetActorLocation() - SpawnLocation).Rotation();
+	SpawnRotation.Pitch += FMath::FRandRange(0.0f, MaxBulletSpread);
+	SpawnRotation.Yaw += FMath::FRandRange(-MaxBulletSpread, MaxBulletSpread);
+	
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Instigator = Pawn;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		
+	AActor* NewProjectile = GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnLocation, SpawnRotation, SpawnParams);
+	
+	return NewProjectile ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+}
---
+++ Source/ActionRoguelike5/AI/RogueBTTask_RangedAttack.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "RogueBTTask_RangedAttack.generated.h"
+
+class ARogueProjectile;
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE5_API URogueBTTask_RangedAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FBlackboardKeySelector TargetActorKey;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	FName MuzzleSocketName;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	TSubclassOf<ARogueProjectile> ProjectileClass;
+	
+	UPROPERTY(EditAnywhere, Category="AI")
+	float MaxBulletSpread{ 5.0f };
+	
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
